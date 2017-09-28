abef3085b33843639d74465f794787799b7718a4
Angellllok <kanameeeee@gmail.com>
2017-09-28 13:13:44 +0300

    g

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
 var express = require('express');
 var todoController = require('./controllers/todoController');
 var app = express();
-app.set('port', (process.env.PORT || 5000));
 
-//For avoidong Heroku $PORT error
-app.get('/', function(request, response) {
-    var result = 'App is running'
-    response.send(result);
-}).listen(app.get('port'), function() {
-    console.log('App is running, server is listening on port ', app.get('port'));
-});
 //set up template engine
 app.set('view engine', 'ejs');
 
@@ -20,3 +12,12 @@ todoController(app);
 //listen to port
 //app.listen(3000);
 //console.log('You are listening to port 3000');
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
