e248b2a118e32823ef68d0dc21447c5c05514750
Angellllok <kanameeeee@gmail.com>
2017-09-28 05:07:41 +0300

    f

--- app.js
+++ app.js
@@ -10,5 +10,6 @@ app.use(express.static('./public'));
 //fire controllers
 todoController(app);
 //listen to port
-app.listen(3000);
-console.log('You are listening to port 3000');
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
