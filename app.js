387afb6377c3c026ed665cc7966dd41dfa68ea1b
Angellllok <kanameeeee@gmail.com>
2017-09-28 03:52:26 +0300

    i

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var todoController = require('./controllers/todoController');
 var app = express();
-
+app.set('port', (process.env.PORT || 5000));
 //set up template engine
 app.set('view engine', 'ejs');
 
@@ -10,5 +10,8 @@ app.use(express.static('./public'));
 //fire controllers
 todoController(app);
 //listen to port
-app.listen(3000);
-console.log('You are listening to port 3000');
+//app.listen(3000);
+//console.log('You are listening to port 3000');
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
