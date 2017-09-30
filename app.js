9e94a40f8869480321a87568d276d101964ea494
Angellllok <kanameeeee@gmail.com>
2017-09-30 12:25:47 +0300

    proga

--- app.js
+++ app.js
@@ -12,18 +12,3 @@ todoController(app);
 //listen to port
 app.listen(3000);
 console.log('You are listening to port 3000');
-const router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-    var token = req.body.token('token');
-    if(!token)
-    {
-        res.render('error', {
-            'message': "You must indicate a Token"
-        });
-    }
-
-});
-
-module.exports = router;
--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,23 +1,46 @@
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
+//Connect to database
+mongoose.connect('mongodb://Angellllok:SS123asdAS@ds147534.mlab.com:47534/mytodo');
 
-var data = [{item: 'take a snack'}, {item: 'read a book'}, {item: 'play game'}];
+//Create a schema
+var todoSchema = new mongoose.Schema({
+  item:String
+})
+
+var Todo = mongoose.model('Todo', todoSchema);
+//var itemOne = Todo({item: 'buy flowers'}).save(function(err){
+//  if (err) throw err;
+//  console.log('item saved');
+//});
+//var data = [{item: 'take a snack'}, {item: 'read a book'}, {item: 'play game'}];
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 module.exports = function(app) {
 
 app.get('/todo', function(req, res){
-  res.render('todo', {todos: data});
-});
-app.post('/todo', urlencodedParser, function(req, res){
-  data.push(req.body);
-  res.json(data);
-
-});
-app.delete('/todo/:item', function(req, res){
-  data = data.filter(function(todo){
-    return todo.item.replace(/ /g, '-') !== req.params.item;
+  //get data from mongodb
+  Todo.find({}, function(err, data){
+    if (err) throw err;
+    res.render('todo', {todos: data});
   });
-res.json(data);
 });
+
+app.post('/todo', urlencodedParser, function(req, res){
+  //get data from the view and add it to mongodb
+  var newTodo = Todo(req.body).save(function(err,data){
+    if (err) throw err;
+    res.json(data);
+  });
+});
+
+app.delete('/todo/:item', function(req, res){
+  //delete requested item from mongodb
+  Todo.find({item: req.params.item.replace(/\-/g, " ")}).remove(function(err,data){
+    if (err) throw err;
+    res.json(data);
+  });
+  });
+  
 };
