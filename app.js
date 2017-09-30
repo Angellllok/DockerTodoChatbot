9f65313f28065bf699c35ca904acd0482f585b2f
Angellllok <kanameeeee@gmail.com>
2017-09-30 14:45:48 +0300

    g

--- app.js
+++ app.js
@@ -5,16 +5,10 @@ var app = express();
 //set up template engine
 app.set('view engine', 'ejs');
 
-//static
+//static files
 app.use(express.static('./public'));
 //fire controllers
-//todoController(app);
-app.get('/todo', function(req, res){
-  //get data from mongodb
-  Todo.find({}, function(err, data){
-    if (err) throw err;
-    res.render('todo', {todos: data});
-  });
+todoController(app);
 //listen to port
 app.listen(process.env.PORT || 3000);
 console.log('You are listening to port 3000');
