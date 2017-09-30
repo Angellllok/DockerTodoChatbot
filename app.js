b1caadb7d013627521292762e2de2fbac8090d34
Angellllok <kanameeeee@gmail.com>
2017-09-30 17:39:22 +0300

    f

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var todoController = require('./controllers/todoController');
 var app = express();
-const router = express.Router();
+
 //set up template engine
 app.set('view engine', 'ejs');
 
 //static files
-app.use(express.static('./public'));
+//app.use(express.static('./public'));
 app.use('/', tokenHandler);
-
-/* GET home page. */
+//fire controllers
+todoController(app);
 router.get('/', function(req, res, next) {
     var token = req.body.token('token');
     if(!token)
@@ -22,8 +22,6 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
-//fire controllers
-todoController(app);
 //listen to port
 app.listen(process.env.PORT || 3000);
 console.log('You are listening to port 3000');
