14011a6857e08db6197c55cd9f0fa71c9e61f2ab
Angellllok <kanameeeee@gmail.com>
2017-09-30 18:22:44 +0300

    f

--- app.js
+++ app.js
@@ -6,22 +6,11 @@ var app = express();
 app.set('view engine', 'ejs');
 
 //static files
-//app.use(express.static('./public'));
-app.use('/', tokenHandler);
+app.use(express.static('./public'));
+
 //fire controllers
 todoController(app);
-router.get('/', function(req, res, next) {
-    var token = req.body.token('token');
-    if(!token)
-    {
-        res.render('error', {
-            'message': "You must indicate a Token"
-        });
-    }
 
-});
-
-module.exports = router;
 //listen to port
 app.listen(process.env.PORT || 3000);
 console.log('You are listening to port 3000');
